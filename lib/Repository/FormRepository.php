37404b447ff0e4fdd6bb41d4ed2f1cc9a9288213
Suleymanov Artur <l0tus0rb@yandex.ru>
2024-04-24 17:00:43 +0200

    FormRepository refactoring

--- lib/Repository/FormRepository.php
+++ lib/Repository/FormRepository.php
@@ -1,8 +1,15 @@
 <?php
+
 namespace Up\Forms\Repository;
 
 use Up\Forms\Model\ChapterTable;
+use Up\Forms\Model\EO_Chapter;
+use Up\Forms\Model\EO_Form;
+use Up\Forms\Model\EO_FormFormSettings;
 use Up\Forms\Model\EO_FormFormSettings_Collection;
+use Up\Forms\Model\EO_Option;
+use Up\Forms\Model\EO_Question;
+use Up\Forms\Model\EO_QuestionQuestionSettings;
 use Up\Forms\Model\FormFormSettingsTable;
 use Up\Forms\Model\FormTable;
 use Up\Forms\Model\OptionTable;
@@ -16,59 +23,11 @@ class FormRepository
 		\db()->startTransaction();
 		try
 		{
-			global $USER;
-			$form = FormTable::createObject();
-			$form->setCreatorId($USER->GetID());
-			$form->setTitle($formData['TITLE']);
-			foreach ($formData['CHAPTER'] as $chapterData)
-			{
-
-				$chapter = ChapterTable::createObject();
-				$chapter->setTitle($chapterData['TITLE']);
-				$chapter->setDescription($chapterData['DESCRIPTION']);
-				foreach ($chapterData['QUESTION'] as $questionData)
-				{
-					if ($questionData === null)
-					{
-						continue;
-					}
-					$question = QuestionTable::createObject();
-					$question->setTitle($questionData['TITLE']);
-					$question->setPosition($questionData['POSITION']);
-					$question->setFieldId($questionData['FIELD_ID']);
-					foreach ($questionData['OPTION'] as $optionData)
-					{
-						if ($optionData === null)
-						{
-							continue;
-						}
-						$option = OptionTable::createObject();
-						$option->setTitle($optionData['TITLE']);
-						$option->setIsRightAnswer($optionData['IS_RIGHT_ANSWER']);
-						$question->addToOption($option);
-					}
-					foreach ($questionData['SETTINGS'] as $questionSettingData)
-					{
-						$questionSetting = QuestionQuestionSettingsTable::createObject();
-						$questionSetting->setSettingsId($questionSettingData['SETTINGS_ID']);
-						$questionSetting->setValue($questionSettingData['VALUE']);
-						$question->addToSettings($questionSetting);
-					}
-					$chapter->addToQuestion($question);
-				}
-				$form->addToChapter($chapter);
-			}
-			foreach ($formData['SETTINGS'] as $formSettingData)
-			{
-				$formSetting = FormFormSettingsTable::createObject();
-				$formSetting->setSettingsId($formSettingData['ID']);
-				$formSetting->setValue($formSettingData['VALUE']);
-				$form->addToSettings($formSetting);
-			}
-			$result = $form->save()->getErrors();
+			$form = self::fillFormByData($formData);
+			$result = $form->save();
 			db()->commitTransaction();
 
-			return $result;
+			return $result->getErrors();
 		}
 		catch (\Throwable $error)
 		{
@@ -80,96 +39,22 @@ class FormRepository
 
 	public static function saveForm($formData)
 	{
-		$form = FormTable::getById($formData['ID'])->fetchObject();
-		$form->setTitle($formData['TITLE']);
-		$form->removeAllChapter();
-		foreach ($formData['CHAPTER'] as $chapterData)
+		\db()->startTransaction();
+		try
 		{
-			$chapter = ChapterTable::wakeUpObject(
-				[
-					'ID' => $chapterData['ID'],
-				]
-			);
-			$chapter->removeAllQuestion();
-			foreach ($chapterData['QUESTION'] as $questionData)
-			{
-				if ($questionData === null)
-				{
-					continue;
-				}
-				if ($questionData['ID'] === null)
-				{
-					$question = QuestionTable::createObject();
-				}
-				else
-				{
-					$question = QuestionTable::wakeUpObject(
-						[
-							'ID' => $questionData['ID'],
-						]
-					);
-					$question->removeAllOption();
-					$question->removeAllSettings();
-				}
-				$question->setTitle($questionData['TITLE']);
-				$question->setPosition($questionData['POSITION']);
-				$question->setFieldId($questionData['FIELD_ID']);
-				foreach ($questionData['OPTION'] as $optionData)
-				{
-					if ($optionData === null)
-					{
-						continue;
-					}
-					if ($optionData['ID'] === null)
-					{
-						$option = OptionTable::createObject();
-					}
-					else
-					{
-						$option = OptionTable::wakeUpObject(
-							[
-								'ID' => $optionData['ID'],
-							]
-						);
+			self::deleteForm($formData['ID']);
 
-					}
-					$option->setTitle($optionData['TITLE']);
-					$option->setIsRightAnswer($optionData['IS_RIGHT_ANSWER']);
-					$option->save();
+			$form = self::fillFormByData($formData);
+			$result = $form->save();
+			db()->commitTransaction();
 
-					$question->addToOption($option);
-				}
-				foreach ($questionData['SETTINGS'] as $questionSettingData)
-				{
-					if ($questionData['ID'] === null)
-					{
-						$questionSetting = QuestionQuestionSettingsTable::createObject();
-						$questionSetting->setSettingsId($questionSettingData['SETTINGS_ID']);
-					}
-					else
-					{
-						$questionSetting = QuestionQuestionSettingsTable::wakeUpObject(
-							['QUESTION_ID' => $questionData['ID'], 'SETTINGS_ID' => $questionSettingData['SETTINGS_ID']]);
-					}
-					$questionSetting->setValue($questionSettingData['VALUE']);
-					$question->addToSettings($questionSetting);
-				}
-				$chapter->addToQuestion($question);
-			}
-			$form->addToChapter($chapter);
+			return $result->getErrors();
 		}
-		$form->removeAllSettings();
-		foreach ($formData['SETTINGS'] as $formSettingData)
+		catch (\Throwable $error)
 		{
-			$formSetting = FormFormSettingsTable::wakeUpObject(
-				['FORM_ID' => $formData['ID'], 'SETTINGS_ID' => $formSettingData['ID']]
-			);
-			$formSetting->setValue($formSettingData['VALUE']);
-			$form->addToSettings($formSetting);
+			db()->rollbackTransaction();
+			throw $error;
 		}
-		$result = $form->save();
-
-		return $result->getErrors();
 	}
 
 	public static function getForm(int $id)/*: EO_Form*/
@@ -193,27 +78,21 @@ class FormRepository
 	{
 		if ($filter === null)
 		{
-			return FormTable::query()
-							->setSelect(['ID', 'TITLE', 'CREATOR_ID'])
-							->fetchAll();
+			return FormTable::query()->setSelect(['ID', 'TITLE', 'CREATOR_ID'])->fetchAll();
 		}
-		return FormTable::query()
-						->setSelect(['ID', 'TITLE', 'CREATOR_ID'])
-						->whereLike('TITLE', '%' . $filter['TITLE'] . '%')
-						->whereIn('CREATOR_ID', $filter['USERS'])
-						->setLimit($filter['LIMIT'])
-						->setOffset($filter['OFFSET'])
-						->fetchAll();
+
+		return FormTable::query()->setSelect(['ID', 'TITLE', 'CREATOR_ID'])->whereLike(
+			'TITLE',
+			'%' . $filter['TITLE'] . '%'
+		)->whereIn('CREATOR_ID', $filter['USERS'])->setLimit($filter['LIMIT'])->setOffset($filter['OFFSET'])->fetchAll(
+		);
 	}
 
-	public static function deleteForm(int $id): void
+	public static function deleteForm(int $id): \Bitrix\Main\ORM\Data\Result
 	{
-		$form = FormTable::getById($id)->fetchObject();
-		$questions = $form->fillChapter()->fillQuestion();
-		$questions->fillOption();
-		$questions->fillAnswer();
+		$form = self::getForm($id);
 
-		$form->delete();
+		return $form->delete();
 	}
 
 	public static function deleteForms(array $ids): void
@@ -227,15 +106,143 @@ class FormRepository
 	public static function getFormSettings(int $id): EO_FormFormSettings_Collection
 	{
 		$settings = \Up\Forms\Model\FormFormSettingsTable::getByPrimary(['FORM_ID' => $id])->fetchCollection();
+
 		return $settings;
 	}
 
 	public static function getMaxNumberOfTry(int $id): ?int
 	{
 		$setting = FormFormSettingsTable::getByPrimary(['FORM_ID' => $id, 'SETTINGS_ID' => 5])->fetchObject();
-		$maxTry = $setting->getValue() === '' ? null : (int) $setting->getValue();
+		$maxTry = $setting->getValue() === '' ? null : (int)$setting->getValue();
 
 		return $maxTry;
+	}
+
+	private static function fillFormByData(array $formData): EO_Form
+	{
+		global $USER;
+
+		$form = FormTable::createObject();
+		if ((int) $formData['ID'] > 0)
+		{
+			$form->setId($formData['ID']);
+		}
+
+		$form->setCreatorId($USER->GetID());
+		$form->setTitle($formData['TITLE']);
+
+		foreach ($formData['CHAPTER'] as $chapterData)
+		{
+			if ($chapterData === null)
+			{
+				continue;
+			}
+			$chapter = self::fillChapterByData($chapterData);
+			$form->addToChapter($chapter);
+		}
+
+		foreach ($formData['SETTINGS'] as $formSettingData)
+		{
+			$setting = self::fillFormSettingByData($formSettingData, $formData['ID']);
+			$form->addToSettings($setting);
+		}
+
+		return $form;
+	}
+
+	private static function fillChapterByData(array $chapterData): EO_Chapter
+	{
+		$chapter = ChapterTable::createObject();
+		if(!is_null($chapterData['ID']))
+		{
+			$chapter->setId($chapterData['ID']);
+		}
+
+		$chapter->setTitle($chapterData['TITLE']);
+		$chapter->setDescription($chapterData['DESCRIPTION']);
+
+		foreach ($chapterData['QUESTION'] as $questionData)
+		{
+			if ($questionData === null)
+			{
+				continue;
+			}
+			$question = self::fillQuestionByData($questionData);
+			$chapter->addToQuestion($question);
+		}
+
+		return $chapter;
+	}
+
+	private static function fillQuestionByData(array $questionData): EO_Question
+	{
+		$question = QuestionTable::createObject();
+		if (!is_null($questionData['ID']))
+		{
+			$question->setId($questionData['ID']);
+		}
+		$question->setTitle($questionData['TITLE']);
+		$question->setPosition($questionData['POSITION']);
+		$question->setFieldId($questionData['FIELD_ID']);
+
+		foreach ($questionData['OPTION'] as $optionData)
+		{
+			if ($optionData === null)
+			{
+				continue;
+			}
+			$option = self::fillOptionByData($optionData);
+			$question->addToOption($option);
+		}
+
+		foreach ($questionData['SETTINGS'] as $settingData)
+		{
+			$setting = self::fillQuestionSettingData($settingData, $questionData['ID']);
+			$question->addToSettings($setting);
+		}
+
+		return $question;
+	}
+
+	private static function fillOptionByData(array $optionData): EO_Option
+	{
+		$option = OptionTable::createObject();
+		if (!is_null($optionData['ID']))
+		{
+			$option->setId($optionData['ID']);
+		}
+
+		$option->setTitle($optionData['TITLE']);
+		$option->setIsRightAnswer($optionData['IS_RIGHT_ANSWER']);
+
+		return $option;
+	}
+
+	private static function fillQuestionSettingData(array $settingData, ?int $questionId): EO_QuestionQuestionSettings
+	{
+		$setting = QuestionQuestionSettingsTable::createObject();
+		$setting->setSettingsId($settingData['SETTINGS_ID']);
+
+		if (!is_null($questionId))
+		{
+			$setting->setQuestionId($questionId);
+		}
+		$setting->setValue($settingData['VALUE']);
+
+		return $setting;
+	}
+
+	private static function fillFormSettingByData(array $settingData, int $formId): EO_FormFormSettings
+	{
+		$setting = FormFormSettingsTable::createObject();
+		$setting->setSettingsId($settingData['ID']);
+		if ($formId > 0)
+		{
+			$setting->setFormId($formId);
+		}
+
+		$setting->setValue($settingData['VALUE']);
+		return $setting;
 	}
 }
 
