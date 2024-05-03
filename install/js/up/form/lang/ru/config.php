608f42b0ae82402a2752fa22f71fe490b5599136
Suleymanov Artur <l0tus0rb@yandex.ru>
2024-05-03 18:36:13 +0200

    add to commit lang config file

---
+++ install/js/up/form/lang/ru/config.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['UP_FORMS_FORM_TIME_IS_UP_MESSAGE'] = 'Время вышло!';
+$MESS['UP_FORMS_FORM_SUBMIT_BUTTON'] = 'ОТПРАВИТЬ';
+$MESS['UP_FORMS_FORM_TRYIES_REMAINING'] = 'У вас осталось попыток';
+$MESS['UP_FORMS_FORM_START_BUTTON'] = 'Начать';
+$MESS['UP_FORMS_FORM_REQUIRED_QUESTION'] = 'Обязательный вопрос';
